035e59633ee2afe00bc3a09d12f3fc090133235d
Lyhak123 <limlyhak.g@gmail.com>
2025-01-29 13:48:18 +0700

    Add files via upload

---
+++ lib/profileandpostdata.dart
@@ -0,0 +1,110 @@
+List<String> profileimages = [
+  "https://preview.redd.it/1keuani8y4481.jpg?width=640&crop=smart&auto=webp&s=3a4a19c46c6d2742f162e188e699c451309a7d14",
+  "https://miro.medium.com/v2/resize:fit:646/1*BxTA9zYVYtTwwWdSm7drOw.jpeg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1fSkv0QAUTfH0q12qVAUwmVCMHEw3sR8Eww&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJNEqoMWRYJnGYT9b1gErsgb2IX06K9HQXNQ&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg_hH-if5JupkkPQfgMOr5-l6LqNiOkgPW-A&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDI4dTK87TtDwze48eum0HC1F8gvrICMUrQg&s",
+  "https://static.wikia.nocookie.net/mobile-legends/images/e/e0/Hero1151-portrait.png/revision/latest?cb=20241021143446",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIuWuIMokvJ-tf_Crqsx_2ewO5_hZLC2K7XA&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5yjKiFv5OGI7REKR-k5zS17tVtrLz4g-cEA&s",
+  "https://static.tvtropes.org/pmwiki/pub/images/mlbb_alucard.png",
+  "https://static.tvtropes.org/pmwiki/pub/images/mlbb_alice.png",
+];
+List<String> profilenames = [
+  "Ben10",
+  "Tom and jerry",
+  "Layla",
+  "Zhuxin",
+  "Suyou",
+  "Chou",
+  "Xavier",
+  "Yin",
+  "Badang",
+  "Alukak",
+  "Alice",
+];
+
+List<String> postimages = [
+  "https://m.media-amazon.com/images/M/MV5BYWVjODZjNDgtYjk4ZS00OTg5LTg5NDQtZDMxZDQ4ZmM5MGJmXkEyXkFqcGc@._V1_.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUeMZJ0jGvpg_5riG1M0YRD8qr8M_aNIMhmg&s",
+  "https://i.namu.wiki/i/enQqszK44CjuGXi2E2h821AQDmlGa-MOHTZyPavkI4RM70Keqj00sMhpmNiedy_BrhxC6XzfkmcYO3zC4-Rpdg.webp",
+  "https://admin.esports.gg/wp-content/uploads/2024/06/When-is-the-new-MLBB-hero-Zhuxin-release-date.jpg",
+  "https://cdn.oneesports.gg/cdn-data/2024/09/MLBB_Suyou.jpeg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSawiX31p_2BuilNIUjK1tru00R7TtPYxu8mA&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRXnrlM_FpVXeefKEM2itmrSpZz5P-F6hc5w&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIoRY3Iz-SgalJKqwPGYyh7A1fxTAOWcNNWg&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-m_u2P-fpMn-R4EW1VCcW_FKRk696BkevCg&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRWMJXDO_3qpDd-O7NzbtC5mFoHh0ismw5w&s",
+  "https://api.duniagames.co.id/api/content/upload/file/11931947751698914151.jpg",
+];
+
+List<String> captionText = [
+  "this show is 10/10",
+  "Top show",
+  "5/10",
+  "Hiiiii",
+  "on my way to ruin someone day",
+  "new look",
+  "new collab skin",
+  "yin in the area",
+  "wanna eat some ",
+  "ambatublow",
+  "i want to eat some coke",
+];
+
+List<String> likedByName = [
+  "hak",
+  "Thy sobroney",
+  "Dom gaming",
+  "mey",
+  "fong",
+  "Dara",
+  "phea",
+  "hour",
+  "yuth",
+  "nang",
+  "kdey",
+];
+
+List<String> datePost = [
+  "7 days ago",
+  "6 hours ago",
+  "2 days ago",
+  "October 27, 2024",
+  "1 day ago",
+  "December 05, 2024",
+  "19 hours ago",
+  "November 28, 2024",
+  "December 06, 2024",
+  "1 day ago",
+  "November 30, 2024",
+];
+
+List<String> dateChat = [
+  "Sent 6h ago",
+  "Sent Wednesday",
+  "Sent Yesterday",
+  "Active now",
+  "Seen last week",
+  "Seen",
+  "Active today",
+  "Active 10m ago",
+  "Sent",
+  "Liked a message",
+  "Seen",
+];
+
+List<String> notiMessage = [
+  "liked your story.",
+  "liked your story.",
+  "commented on your post.",
+  "commented on your post.",
+  "liked your post.",
+  "liked your post.",
+  "shared a photo.",
+  "shared a photo.",
+  "mentioned you in a comment.",
+  "mentioned you in a comment.",
+  "liked your story.",
+];
